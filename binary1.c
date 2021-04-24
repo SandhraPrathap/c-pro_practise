06cfd3f0edb12dc85397b420077f6274877aeea6
Sandhra <sandhraprathap@gmail.com>
2021-04-24 19:34:19 +0530

    Program to find nuisance number

---
+++ binary1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int n,x,k=0;
+    printf("Enter n\n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {k=0;
+        printf("Enter x\n");
+        scanf("%d",&x);
+    while(x!=0)
+    {
+        if(x%2==1)
+        k++;
+        x/=2;
+
+    }
+    printf("%d\n",k);}
+}
