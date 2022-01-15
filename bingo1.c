9558d170524082a5da812c4d55fca35344bd3884
dino <sandhraprathap@gmail.com>
2022-01-15 23:41:39 +0530

    bingo prg modified

--- bingo1.c
+++ bingo1.c
@@ -1,53 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+void check(int a[50][50],int b){
+    int i=0,j=0;
+for (i = 0; i < 5; i++)
+        {
+            for (j = 0; j < 5; j++)
+                if (b == a[i][j])
+                {
+                    a[i][j] = 0;
+                    break;
+                }
+        }
+}
 void main()
 {
     int a[5][5];
-    int randoma[5][5];
-    int i, j, t = 0, b, m = 0, n = 0, d = 0, o = 0, u = 1;
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            randoma[i][j] = u;
-            u++;
-        }
-    }
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            printf("%d ", randoma[i][j]);
-        }
-        printf("\n");
-    }
-    for (i = 0; i < 5; i++)
-    {
-            int temp = randoma[i][j];
-            int randomIndex = rand() % 25;
-            printf("%d\n", randomIndex);
-            printf("%d\n", temp);
-            int randomIndex1 = rand() % 25;
-
-            randoma[i][j] = randoma[randomIndex][randomIndex1];
-            randoma[randomIndex][randomIndex1] = temp;
-        
-    }
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            printf("%d ", randoma[i][j]);
-        }
-        printf("\n");
-    }
+    int i, j, t = 0, b, m = 0, n = 0, d = 0, o = 0;
     printf("Enter the matrix\n");
     for (i = 0; i < 5; i++)
     {
         for (j = 0; j < 5; j++)
             scanf("%d", &a[i][j]);
     }
-
     while (1)
     {
         d = 0;
@@ -57,26 +30,9 @@ void main()
         n = 0;
         printf("Player 1\n");
         scanf("%d", &b);
-        for (i = 0; i < 5; i++)
-        {
-            for (j = 0; j < 5; j++)
-                if (b == a[i][j])
-                {
-                    a[i][j] = 0;
-                    break;
-                }
-        }
+        check(a,b);
         printf("Player 2\n");
-        scanf("%d", &b);
-        for (i = 0; i < 5; i++)
-        {
-            for (j = 0; j < 5; j++)
-                if (b == a[i][j])
-                {
-                    a[i][j] = 0;
-                    break;
-                }
-        }
+        check(a,b);
         for (i = 0; i < 5; i++)
         {
             t = 0;
@@ -103,7 +59,7 @@ void main()
                 {
                     n++;
                 }
-                if (a[i][4 - i] == 0)
+                if (a[i][4-i] == 0)
                 {
                     o++;
                 }
