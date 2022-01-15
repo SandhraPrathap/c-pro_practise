2747d8b4194392dac55ba34229034150f5225822
dino <sandhraprathap@gmail.com>
2022-01-16 00:30:05 +0530

    trynna create random matrix

--- bingo.c
+++ bingo.c
@@ -3,44 +3,8 @@
 void main()
 {
     int a[5][5];
-    int randoma[5][5];
     int i, j, t = 0, b, m = 0, n = 0, d = 0, o = 0, u = 1;
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            randoma[i][j] = u;
-            u++;
-        }
-    }
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            printf("%d ", randoma[i][j]);
-        }
-        printf("\n");
-    }
-    for (i = 0; i < 5; i++)
-    {
-            int temp = randoma[i][j];
-            int randomIndex = rand() % 25;
-            printf("%d\n", randomIndex);
-            printf("%d\n", temp);
-            int randomIndex1 = rand() % 25;
-
-            randoma[i][j] = randoma[randomIndex][randomIndex1];
-            randoma[randomIndex][randomIndex1] = temp;
-        
-    }
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            printf("%d ", randoma[i][j]);
-        }
-        printf("\n");
-    }
+    
     printf("Enter the matrix\n");
     for (i = 0; i < 5; i++)
     {
